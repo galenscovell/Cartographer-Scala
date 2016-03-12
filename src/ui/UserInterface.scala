e68e45101563ef7bbf2885c07e60944197d54b02
Galen <galen.scovell@gmail.com>
2016-03-11 19:20:10 -0800

    Setup main application window

---
+++ src/ui/UserInterface.scala
@@ -0,0 +1,9 @@
+package ui
+
+import scala.swing._
+
+class UserInterface extends MainFrame {
+  title = "Prim's Algorithm"
+  preferredSize = new Dimension(800, 480)
+  contents = new Label("Content Label")
+}
