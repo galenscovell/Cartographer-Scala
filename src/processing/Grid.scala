a416c55e63f6cb0ef71ec3384bf7a8f1c14a68c8
Galen <galen.scovell@gmail.com>
2016-03-12 21:41:10 -0800

    Begin Prim's algorithm w/ frontier and mark

--- src/processing/Grid.scala
+++ src/processing/Grid.scala
@@ -2,19 +2,75 @@ package processing
 
 import java.awt.Color
 
-class Grid(val width: Int, val height: Int, val nodeSize: Int) {
-  private val gridWidth = width / nodeSize
-  private val gridHeight = height / nodeSize
-  private val nodeGrid = Array.ofDim[Node](gridWidth, gridHeight)
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
+class Grid (val width: Int, val height: Int, val nodeSize: Int) {
+  private val gridWidth: Int = width / nodeSize
+  private val gridHeight: Int = height / nodeSize
+  private val nodes: Array[Array[Node]] = Array.ofDim[Node](gridWidth, gridHeight)
+  private var frontier: ArrayBuffer[Node] = ArrayBuffer()
+  private val random: Random = Random
+
+  // Generate grid full of walls
   for (x <- 0 until gridWidth) {
     for (y <- 0 until gridHeight) {
-      val newNode = new Node(x, y, nodeSize)
-      newNode.color = new Color(100, x * 2, y * 2)
-      nodeGrid(x)(y) = newNode
+      val newNode: Node = new Node(x, y, nodeSize)
+      // newNode.color = new Color(100, x * 2, y * 2)
+      nodes(x)(y) = newNode
     }
   }
-  println("%d %d".format(nodeGrid.length, nodeGrid(0).length))
 
-  def getGrid(): Array[Array[Node]] = nodeGrid
+  // Randomly pick first maze node
+  val startX: Int = random.nextInt(gridWidth)
+  val startY: Int = random.nextInt(gridHeight)
+  mark(startX, startY)
+
+
+  private def mark(x: Int, y: Int) = {
+    nodes(x)(y).color = Color.WHITE
+
+    for (dx <- -1 to 1 by 2) {
+      val newX: Int = x + dx
+      if (!isOutOfBounds(newX, y)) {
+        val neighborNode: Node = nodes(newX)(y)
+        if (neighborNode.isWall()) {
+          frontier += neighborNode
+        }
+      }
+    }
+
+    for (dy <- -1 to 1 by 2) {
+      val newY: Int = y + dy
+      if (!isOutOfBounds(x, newY)) {
+        val neighborNode: Node = nodes(x)(newY)
+        if (neighborNode.isWall()) {
+          frontier += neighborNode
+        }
+      }
+    }
+  }
+
+
+  def expandFrontier() = {
+    if (frontier.length > 0) {
+      val randomIndex: Int = random.nextInt(frontier.length)
+      val neighborNode: Node = frontier(randomIndex)
+      frontier -= neighborNode
+      mark(neighborNode.x, neighborNode.y)
+      true
+    } else {
+      false
+    }
+  }
+
+
+  private def isOutOfBounds(x: Int, y: Int) = {
+    x < 0 || x >= gridWidth || y < 0 || y >= gridHeight
+  }
+
+
+  def getGrid(): Array[Array[Node]] = {
+    nodes
+  }
 }
--- src/processing/Node.scala
+++ src/processing/Node.scala
@@ -2,11 +2,17 @@ package processing
 
 import java.awt.Color
 
-class Node(val xPos: Int, val yPos: Int, val nodeSize: Int) {
+class Node (val xPos: Int, val yPos: Int, val nodeSize: Int) {
   val x: Int = xPos
   val y: Int = yPos
   val exactX: Int = xPos * nodeSize
   val exactY: Int = yPos * nodeSize
   val size: Int = nodeSize
-  var color: Color = Color.WHITE
+  // Black is wall, white is maze
+  var color: Color = Color.BLACK
+
+
+  def isWall() = {
+    color == Color.BLACK
+  }
 }
